c4413dff1eb32c5dc978e424b72edd8ba4dfbffe
Andrew Le <s3820098@rmit.edu.vn>
2022-08-12 21:09:45 +0700

    commit

---
+++ ThreeDucks/ThreeDucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreeDucksApp.swift
+//  ThreeDucks
+//
+//  Created by Andrew Le Nguyen on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ThreeDucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
